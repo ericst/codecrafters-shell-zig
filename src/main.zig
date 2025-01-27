4cada5885a4811e2882f3e5103e9601fed1d6f38
Eric Seuret <eric@ericst.ch>
2025-01-27 10:36:56 +0100

    REPL

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Uncomment this block to pass the first stage
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
+    while (true) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
-    // TODO: Handle user input
+        // TODO: Handle user input
 
-    const stderr = std.io.getStdErr().writer();
-    try stderr.print("{s}: command not found\n", .{user_input});
+        const stderr = std.io.getStdErr().writer();
+        try stderr.print("{s}: command not found\n", .{user_input});
+    }
 }
